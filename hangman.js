851faccda1916ed2786d13f301d0752ad4b79828
dewan765 <dewansadik41@gmail.com>
2025-10-22 10:16:58 -0400

    Added changing image funtionality

--- hangman.js
+++ hangman.js
@@ -1,17 +1,24 @@
 var POSSIBLE_WORDS = ["obdurate, verisimilitude", "defenestrate", 
     "obsequious", "dissonant", "toady", "idempotent"];
+var MAX_GUESSES = 6;
 var word = "";
 var guesses = "";
+var guess_count = MAX_GUESSES;
+
 function newGame() {
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
+    guess_count = MAX_GUESSES;
     updatePage();
 }
 
 function guessLetter(){
     var input = document.getElementById("guess");
     var letter = input.value;
+    if (word.indexOf(letter)< 0) {
+        guess_count--;
+    }
     guesses += letter;
     updatePage();
 }
@@ -26,9 +33,15 @@ function updatePage() {
         else
             clueString += "_ ";
     }
+    //update the clue string
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    //update the guesses from the user
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+    //update the image
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count + ".gif";
 }
